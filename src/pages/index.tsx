9768b4d7b08407b791b49fe1a33ca8fe68f0f272
sideshow-charles <charlesonuoha97@gmail.com>
2024-11-20 11:41:31 +0100

    Feat(App): App Home Screen Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
 import Header from "@/components/header"
+import Button from "@/components/UI/button"
 
 
 export default function Home() {
   return (
     <div id="app">
       <Header></Header>
+
+      <div className="hero__section">
+        <p className="hero__section__text">Instantly convert currencies with real-time rates and set conversions for specific times. Stay up-to-date and plan your exchanges with confidence.</p>
+        <Button className="Button" content="Konvert!"></Button>
+      </div>
     </div>
   );
 }
